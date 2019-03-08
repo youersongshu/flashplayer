7eadb167ef962170438d6c5568f366558131fc16
zhangjunbao <zhangjunbao@yunke.com>
2019-03-08 14:40:00 +0800

    update

--- local.php
+++ local.php
@@ -6,6 +6,6 @@
 // database config
 define('DB_HOST','10.10.69.169');
 define('DB_USERNAME','root');
-define('DB_PASSWORD','yhnji-db-yoqoo');
+define('DB_PASSWORD','db_passwd');
 define('DB_NAME','testcase_player');
 ?>
